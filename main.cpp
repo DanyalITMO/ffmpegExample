2e3d80f9719f118401954769c46921f36201031e
DanyalITMO <mugutdinov.danyal@yandex.ru>
2018-02-14 02:03:31 +0300

    nb streams print

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ extern "C"
 }
 
 #include <string>
+#include <iostream>
 
 int main()
 {
@@ -23,46 +24,11 @@ int main()
       //  CloseFile();
         return false;
     }
-    av_dump_format(pFormatCtx, 0,  inputFile.c_str(), 0);
+    //av_dump_format(pFormatCtx, 0,  inputFile.c_str(), 0);
 
 
-    //---
-    int i;
-    AVCodecContext *pCodecCtxOrig = NULL;
-    AVCodecContext *pCodecCtx = NULL;
-
-// Find the first video stream
-    int videoStream=-1;
-    for(i=0; i<pFormatCtx->nb_streams; i++)
-        if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO) {
-            videoStream=i;
-            break;
-        }
-    if(videoStream==-1)
-        return -1; // Didn't find a video stream
-
-// Get a pointer to the codec context for the video stream
-    pCodecCtx=pFormatCtx->streams[videoStream]->codec;
-
-    //----
+    std::cout<<pFormatCtx->nb_streams<<std::endl;
+    std::cout<<pFormatCtx->nb_streams<<std::endl;
 
 
-    AVCodec *pCodec = nullptr;
-
-// Find the decoder for the video stream
-    pCodec=avcodec_find_decoder(pCodecCtx->codec_id);
-    if(pCodec==NULL) {
-        fprintf(stderr, "Unsupported codec!\n");
-        return -1; // Codec not found
-    }
-// Copy context
-    pCodecCtx = avcodec_alloc_context3(pCodec);
-
-    if(avcodec_copy_context(pCodecCtx, pCodecCtxOrig) != 0) {
-        fprintf(stderr, "Couldn't copy codec context");
-        return -1; // Error copying codec context
-    }
-// Open codec
-    if(avcodec_open2(pCodecCtx, pCodec, nullptr)<0)
-        return -1; // Could not open codec
 }
