2bde7a0a9ba07bea1396563fd1efc41ad608bd7c
DanyalITMO <mugutdinov.danyal@yandex.ru>
2018-02-15 01:20:52 +0300

    simple change

--- main.cpp
+++ main.cpp
@@ -6,29 +6,49 @@ extern "C"
 
 #include <string>
 #include <iostream>
+#include <vector>
 
-int main()
-{
+
+int main() {
     av_register_all();
 
-    AVFormatContext* pFormatCtx = nullptr;
+    AVFormatContext *pFormatCtx = nullptr;
     std::string inputFile = "/home/danek/Downloads/videoplayback.mp4";
-    if (avformat_open_input(&pFormatCtx, inputFile.c_str(), NULL, 0) != 0)
-    {
+    if (avformat_open_input(&pFormatCtx, inputFile.c_str(), NULL, 0) != 0) {
         //CloseFile();
         return false;
     }
 // Get infromation about streams
-    if (avformat_find_stream_info(pFormatCtx, NULL) < 0)
-    {
-      //  CloseFile();
+    if (avformat_find_stream_info(pFormatCtx, NULL) < 0) {
+        //  CloseFile();
         return false;
     }
     //av_dump_format(pFormatCtx, 0,  inputFile.c_str(), 0);
 
 
-    std::cout<<pFormatCtx->nb_streams<<std::endl;
-    std::cout<<pFormatCtx->nb_streams<<std::endl;
+    std::cout << pFormatCtx->nb_streams << std::endl;
+    std::cout << pFormatCtx->nb_streams << std::endl;
+
+    std::vector<AVStream *> streams;
+    for (auto i = 0u; i < pFormatCtx->nb_streams; i++)
+        streams.push_back(pFormatCtx->streams[i]);
+
+
+    AVPacket *packet;
+    AVFormatContext *c = nullptr;
+    for (auto it : streams) {
+        int ret;
+        if (it->codec->coder_type == AVMEDIA_TYPE_VIDEO) {
+            do {
+                ret = av_read_frame(pFormatCtx, packet);
+                av_write_frame(c, packet);
+
+            } while (ret == 1);
+
+        }
+    }
+
+//    av_dump_format()
 
 
 }
